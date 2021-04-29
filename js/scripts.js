7b5170095706d1af0e60b4d3e2e0ad6e4b188561
steffy <stephanie.czapla@gmail.com>
2021-04-29 14:10:04 -0400

    Cleaned up JS code linter told me to

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,10 @@
-console.log("I am invincible in these sunglasses. I am the Fonz, I am the jack of hearts.")
+console.log('I am invincible in these sunglasses. I am the Fonz, I am the jack of hearts.')
 
-function menuToggle() {
-  var x = document.getElementById('myNavtoggle');
+function menuToggle () {
+  var x = document.getElementById('myNavtoggle')
   if (x.className === 'navtoggle') {
-    x.className += ' responsive';
+    x.className += ' responsive'
   } else {
-    x.className = 'navtoggle';
+    x.className = 'navtoggle'
   }
 }
